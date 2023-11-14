b931428a1d3c60b0857c901613b44137c1aace3b
atillasuylu <atillasuylu@gmail.com>
2023-11-14 16:47:13 +0100

    Snap hinzugefügt

--- Assets/Scripts/DnD_Script.cs
+++ Assets/Scripts/DnD_Script.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Transform zone1; // Die Zone, in der sich das Objekt nicht befinden soll, um einen Klon zu erstellen
@@ -13,15 +12,16 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     public void OnPointerDown(PointerEventData eventData)
     {
+
+        if (!IsInZone(zone1))
+        {
         isDragging = true;
         initialPosition = transform.position;
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        if (!IsInZone(zone1)) 
-        {
             CreateClone();
         }
-        if(!IsInZone(zone2)){
+        if (!IsInZone(zone2))
+        {
             Destroy(clone);
         }
     }
@@ -32,7 +32,7 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
         if (clone != null)
         {
-            if (IsInZone(zone2)) 
+            if (IsInZone(zone2))
             {
                 if (IsAlreadyOccupied(zone2))
                 {
@@ -40,7 +40,9 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
                 }
                 else
                 {
+                    // Snap to the center of zone2
                     transform.position = initialPosition;
+                    clone.transform.position = GetCenterOfZone(zone2);
                 }
             }
             else
@@ -98,4 +100,15 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         }
         return false;
     }
+
+    Vector3 GetCenterOfZone(Transform zone)
+    {
+        Renderer renderer = zone.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            return renderer.bounds.center;
+        }
+        return zone.position;
+    }
+
 }
