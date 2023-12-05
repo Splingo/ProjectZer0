2b7587ac4c075c4ac1bf5ad3aa221aff3f0126c3
lost-2101 <lorenz.streicher@gmail.com>
2023-12-05 23:54:30 +0100

    reworked enemy spawning; Enemies now spawning in waves until stopped

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,20 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public string enemyLayerName; // Set this in the Inspector for each spawner
+    public TextMeshProUGUI startFightButtonText;
+    public float spawnInterval = 6f;
+    public bool waiting = false;
+    
 
+
+    // Spawner spawns Enemy every X seconds (spawnInterval)
+    private IEnumerator SpawnEnemyWithInterval()
+    {
+        while (true)
+        {
+            if (waiting == false)
+            {
+                // Spawn enemy on spawner
+                GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+
+                // Convert layer name to layer index
+                int layerIndex = LayerMask.NameToLayer(enemyLayerName);
+
+                // Set the layer of the spawned enemy
+                enemy.layer = layerIndex;
+
+                waiting = true;
+                yield return new WaitForSeconds(spawnInterval);
+            }
+            else
+            {
+                yield return new WaitForSeconds(spawnInterval);
+                waiting = false;
+            }
+        }
+    }
+
+    // function called by Button on canvas
     public void SpawnEnemy()
     {
-        GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-
-        // Convert layer name to layer index
-        int layerIndex = LayerMask.NameToLayer(enemyLayerName);
-
-        // Set the layer of the spawned enemy
-        enemy.layer = layerIndex;
+        Debug.Log(startFightButtonText.text);
+        if (startFightButtonText.text == "Start Fight")
+        {
+            StartCoroutine(SpawnEnemyWithInterval());
+        }
+        else
+        {
+            StopAllCoroutines();
+        }
+        
     }
 }
