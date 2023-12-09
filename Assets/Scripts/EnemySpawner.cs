2845364f9429038ba1a8c0fd1f4350cd85cc8d16
Splingo <sebastianlillge@gmail.com>
2023-12-09 12:02:00 +0100

    Removed unused using. Added review comments.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,9 +7,9 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab;
     public string enemyLayerName; // Set this in the Inspector for each spawner
     public TextMeshProUGUI startFightButtonText;
+    // Review: write '6.0f' to clearly indicate usage of float. (open for discussion :D )
     public float spawnInterval = 6f;
     public bool waiting = false;
-    
 
 
     // Spawner spawns Enemy every X seconds (spawnInterval)
@@ -18,8 +17,10 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
+            // Review: Use '!waiting'? Sounds still understandable to me. 
             if (waiting == false)
             {
+                // Review: Move enemy spawn to own function -> better reusability
                 // Spawn enemy on spawner
                 GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
 
@@ -30,6 +31,9 @@ public class EnemySpawner : MonoBehaviour
                 enemy.layer = layerIndex;
 
                 waiting = true;
+                // Review: Are we not waiting 2xspawnInterval time due to the next line?
+                // Loop goes like this:
+                // if -> spawn enemies -> waiting = true -> wait 6 secs -> next iteration -> else -> wait 6 secs -> waiting = false -> return to beginning?
                 yield return new WaitForSeconds(spawnInterval);
             }
             else
@@ -40,7 +44,9 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
-    // function called by Button on canvas
+    // Maybe find a better name for this? 'ToggleEnemySpawn' or something similar. With the current name I would expect this spawns enemies directly,
+    // but it only toggles the spawn on/off.
+    // Function called by Button on canvas
     public void SpawnEnemy()
     {
         if (startFightButtonText.text == "Start Fight")
@@ -52,6 +58,5 @@ public class EnemySpawner : MonoBehaviour
             StopAllCoroutines();
             waiting = false;
         }
-        
     }
 }
