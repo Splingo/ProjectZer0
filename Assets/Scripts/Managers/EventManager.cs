1aa6ab637ad7b33adb2628b4ea7bd9c2ac211551
Splingo <sebastianlillge@gmail.com>
2024-04-28 16:00:32 +0200

    Removed unused comment

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
-    // This class is used to define UnityEvents that we can use to 
     /// <summary>
     /// This event can be used when an enemy has been killed by the player
     /// </summary>
